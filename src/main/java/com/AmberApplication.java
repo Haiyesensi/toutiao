122249e06a8151a455bcae65deba08872e323e7d
Haiyesensi <32067085+Haiyesensi@users.noreply.github.com>
2018-06-26 22:51:54 +0800

    extends SpringBootServletInitializer and repakage.

--- src/main/java/com/AmberApplication.java
+++ src/main/java/com/AmberApplication.java
@@ -2,11 +2,18 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AmberApplication {
+public class AmberApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(AmberApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(AmberApplication.class);
     }
 }
